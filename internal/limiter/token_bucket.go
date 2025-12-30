fd8ae5c1d561d586e3c3afdf193d20e11e170f0b
JagTheFriend <JagTheFriend12@gmail.com>
2025-12-30 11:20:39 +0530

    Created a limiter function which checks if the client has reach the boundary.

---
+++ internal/limiter/token_bucket.go
@@ -0,0 +1,56 @@
+package limiter
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"go-rate-limiter/internal/storage"
+)
+
+type TokenBucket struct {
+	Redis    *storage.Redis
+	Capacity int
+	Refill   int
+	Interval time.Duration
+}
+
+func (tb *TokenBucket) Allow(ctx context.Context, key string) (bool, int, error) {
+	now := time.Now().Unix()
+
+	tokenKey := "tokens:" + key
+	timeKey := "last:" + key
+
+	lastTimeStr, _ := tb.Redis.Get(ctx, timeKey)
+	lastTime, _ := strconv.ParseInt(lastTimeStr, 10, 64)
+
+	elapsed := now - lastTime
+	refillTokens := int(elapsed/int64(tb.Interval.Seconds())) * tb.Refill
+
+	tokenStr, _ := tb.Redis.Get(ctx, tokenKey)
+	tokens, _ := strconv.Atoi(tokenStr)
+
+	if tokens == 0 {
+		tokens = tb.Capacity
+	}
+
+	tokens = min(tb.Capacity, tokens+refillTokens)
+
+	if tokens <= 0 {
+		return false, 0, nil
+	}
+
+	tokens--
+
+	tb.Redis.Set(ctx, tokenKey, tokens, int64(tb.Interval.Seconds()))
+	tb.Redis.Set(ctx, timeKey, now, int64(tb.Interval.Seconds()))
+
+	return true, tokens, nil
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
